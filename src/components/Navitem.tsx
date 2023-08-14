10f5b2895e34ac2e9669698199777b044fac5b02
fined000@naver.com <fined000@naver.com>
2023-08-14 22:49:28 +0900

    nextauth setting

--- src/components/Navitem.tsx
+++ src/components/Navitem.tsx
@@ -1,8 +1,12 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Navitem({
   mobile,
 }: {mobile?: boolean}) {
+  const {data: session, status} = useSession();
+
+  console.log({session}, status);
   return (
     <>
       <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && 'flext-col h-full'}`}>
@@ -12,12 +16,14 @@ export default function Navitem({
         <li className="py-2 text-center border-b-4 cursor-pointer">
           <Link href='/user'>User</Link>
         </li>
-        <li className="py-2 text-center border-b-4 cursor-pointer">
-          <button>Sign-out</button>
-        </li>
-        <li className="py-2 text-center border-b-4 cursor-pointer">
-          <button>Sign-in</button>
-        </li>
+        {session?.user ? 
+          (<li className="py-2 text-center border-b-4 cursor-pointer">
+            <button onClick={() => signOut()}>Sign-out</button>
+          </li>)
+          :
+          (<li className="py-2 text-center border-b-4 cursor-pointer">
+            <button onClick={() => signIn()}>Sign-in</button></li>)
+        }
       </ul>
     </>
   )
