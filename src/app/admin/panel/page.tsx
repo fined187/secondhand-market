f4f65143afddbb925d206c313f2d3443a8865b45
fined000@naver.com <fined000@naver.com>
2023-08-15 18:42:53 +0900

    next auth session

---
+++ src/app/admin/panel/page.tsx
@@ -0,0 +1,9 @@
+const AdminPanelPage = () => {
+  return (
+    <div>
+      <h1>Page</h1>
+    </div>
+  );
+}
+
+export default AdminPanelPage;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -43,8 +43,16 @@ export const authOptions: NextAuthOptions =
   ],
   session: {
     strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user}) {
+      return {...token, ...user}
+    },
+    async session({ session, token }) {
+      session.user = token;
+      return session;
+    }
   }
 };
-
 
 export default NextAuth(authOptions);
